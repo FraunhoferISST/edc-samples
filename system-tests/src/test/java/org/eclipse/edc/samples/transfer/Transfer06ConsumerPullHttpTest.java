99b440169c0f15b4d7916b8209c8e07dc20908b1
Konstantin Frohnert <konstantin-vincent.frohnert@isst.fraunhofer.de>
2023-09-11 17:19:07 +0200

    Updated Tests for Sample 06

--- system-tests/src/test/java/org/eclipse/edc/samples/transfer/Transfer06ConsumerPullHttpTest.java
+++ system-tests/src/test/java/org/eclipse/edc/samples/transfer/Transfer06ConsumerPullHttpTest.java
@@ -5,8 +5,18 @@ import org.eclipse.edc.junit.extensions.EdcRuntimeExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.time.Duration;
 import java.util.Map;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.After;
+import org.junit.Before;
 
 import static io.restassured.RestAssured.given;
 import static org.awaitility.Awaitility.await;
@@ -19,8 +29,12 @@ public class Transfer06ConsumerPullHttpTest {
 
     static final String PROVIDER_CONFIG_PROPERTIES_FILE_PATH = "transfer/transfer-06-consumer-pull-http/http-pull-provider/provider-configuration.properties";
     static final String SAMPLE_ASSET_FILE_PATH = "transfer/transfer-06-consumer-pull-http/README.md";
-    static final Duration DURATION = Duration.ofSeconds(15);
-    static final Duration POLL_INTERVAL = Duration.ofMillis(500);
+
+    private static String authCode="";
+    private static InputStream serverLogStream=null;
+    private static Logger logger=null;
+
+    private Process serverProcess;
 
     @RegisterExtension
     static EdcRuntimeExtension provider = new EdcRuntimeExtension(
@@ -44,6 +58,38 @@ public class Transfer06ConsumerPullHttpTest {
 
     final ConsumerPullSampleTestCommon testUtils =new ConsumerPullSampleTestCommon();
 
+    @Before
+    public void startServer() throws Exception {
+        String command = "HTTP_SERVER_PORT=4000 java -jar util/http-request-logger/build/libs/http-request-logger.jar";
+        serverProcess = Runtime.getRuntime().exec(command);
+        Thread.sleep(2000);
+
+        logger= Logger.getLogger("HTTPServerLogger");
+        serverLogStream = serverProcess.getInputStream();
+    }
+
+    private static String findAuthCode(InputStream logStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(logStream));
+        String line;
+        Pattern pattern = Pattern.compile("\"authCode\": \"(.*?)\"");
+        String authCode=null;
+
+        while ((line = reader.readLine()) != null) {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.find()) {
+                authCode= matcher.group(1);
+                break;
+            }
+        }
+
+        return authCode;
+    }
+
+    @After
+    public void stopServer() {
+        serverProcess.destroy();
+    }
+
     @Test
     void runSampleSteps() throws Exception {
         testUtils.createAsset();
@@ -54,6 +100,8 @@ public class Transfer06ConsumerPullHttpTest {
         testUtils.getContractAgreement(testUtils.ContractNegotiationId);
         testUtils.createTransferProcess(testUtils.ContractAgreementId);
         testUtils.getTransferProcess(testUtils.TransferProcessId);
+        authCode=findAuthCode(serverLogStream);
+        testUtils.pullData(authCode);
     }
 
 
