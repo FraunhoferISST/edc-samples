ffb80af73503e9c817dad3e4c53843c6e70470f7
Julia Pampus <julia.pampus@isst.fraunhofer.de>
2023-09-22 18:55:36 +0200

    refactor: merge consumer modules to :policy-02-provision:policy-provision-consumer

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -62,12 +62,10 @@ include(":policy:policy-02-provision:policy-provision-provider")
 
 //policy
 
-include(":policy:policy-02-provision:policy-provision")
+include(":policy:policy-02-provision:policy-provision-consumer")
 include(":policy:policy-02-provision:policy-transfer-file-local")
 include(":policy:policy-02-provision:policy-provision-provider-policy-functions")
-include(":policy:policy-02-provision:policy-provision-consumer-policy-functions")
 include(":policy:policy-02-provision:policy-provision-provider")
-include(":policy:policy-02-provision:policy-provision-consumer")
 include(":policy:policy-02-provision:policy-provision-integration-tests")
 
 // modules for code samples ------------------------------------------------------------------------
